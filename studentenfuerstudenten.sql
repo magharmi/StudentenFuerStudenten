dc28309114ec1b5ddaa751fbf9f31fbd693c1909
Goregg <39563843+Goregg@users.noreply.github.com>
2018-06-28 20:43:52 +0200

    Datenbank Aufbau

--- studentenfuerstudenten.sql
+++ studentenfuerstudenten.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 26. Jun 2018 um 16:19
--- Server-Version: 10.1.9-MariaDB
--- PHP-Version: 5.6.15
+-- Erstellungszeit: 28. Jun 2018 um 20:43
+-- Server-Version: 10.1.33-MariaDB
+-- PHP-Version: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,32 +25,123 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `benutzer`
+-- Tabellenstruktur für Tabelle `aufgabe`
 --
 
-CREATE TABLE `benutzer` (
-  `benutzername` varchar(255) NOT NULL,
-  `passwort` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+CREATE TABLE `aufgabe` (
+  `aufgabenID` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `beschreibung` varchar(255) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `kursID` int(11) NOT NULL,
+  `datei` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `benutzer`
---
-
-INSERT INTO `benutzer` (`benutzername`, `passwort`, `email`) VALUES
-('', '$2y$10$AXnWqIgz8m7w1o/TBl0QculzsVk8qeuOeyKINBskACCy4x8OUXBr6', 'ar@gg.com'),
-('', '1234', 'das@gmail.com'),
-('ein deutiger Nutzername', 'SicheresPassw0rt', 'test@email.com');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `todolist`
+-- Tabellenstruktur für Tabelle `kurs`
 --
 
-CREATE TABLE `todolist` (
-  `aufgabe` varchar(255) NOT NULL
+CREATE TABLE `kurs` (
+  `kursID` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `beschreibung` varchar(1024) NOT NULL,
+  `voraussetzung` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `nachhilfeangebot`
+--
+
+CREATE TABLE `nachhilfeangebot` (
+  `angebotID` int(11) NOT NULL,
+  `kursID` int(11) NOT NULL,
+  `titel` varchar(255) NOT NULL,
+  `ort` varchar(255) NOT NULL,
+  `zeit` varchar(255) NOT NULL,
+  `preis` decimal(10,0) NOT NULL,
+  `beschreibung` varchar(255) NOT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `nachhilfesuche`
+--
+
+CREATE TABLE `nachhilfesuche` (
+  `sucheID` int(11) NOT NULL,
+  `kursID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `titel` varchar(255) NOT NULL,
+  `ort` varchar(255) NOT NULL,
+  `zeit` varchar(255) NOT NULL,
+  `preis` decimal(10,0) NOT NULL,
+  `beschreibung` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `todoliste`
+--
+
+CREATE TABLE `todoliste` (
+  `todoID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `beschreibung` int(11) NOT NULL,
+  `checked` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(255) NOT NULL,
+  `passwort` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `uni` varchar(255) NOT NULL,
+  `fach` varchar(255) NOT NULL,
+  `beschreibung` varchar(1024) NOT NULL,
+  `bild` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`name`, `passwort`, `email`, `userID`, `uni`, `fach`, `beschreibung`, `bild`) VALUES
+('ein deutiger Nutzername', 'SicheresPassw0rt', 'test@email.com', 1, '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `useraufgabe`
+--
+
+CREATE TABLE `useraufgabe` (
+  `userID` int(11) NOT NULL,
+  `aufgabenID` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `userkurse`
+--
+
+CREATE TABLE `userkurse` (
+  `userID` int(11) NOT NULL,
+  `kursID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -56,16 +149,83 @@ CREATE TABLE `todolist` (
 --
 
 --
--- Indizes für die Tabelle `benutzer`
+-- Indizes für die Tabelle `aufgabe`
 --
-ALTER TABLE `benutzer`
+ALTER TABLE `aufgabe`
+  ADD PRIMARY KEY (`aufgabenID`);
+
+--
+-- Indizes für die Tabelle `kurs`
+--
+ALTER TABLE `kurs`
+  ADD PRIMARY KEY (`kursID`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indizes für die Tabelle `nachhilfeangebot`
+--
+ALTER TABLE `nachhilfeangebot`
+  ADD PRIMARY KEY (`angebotID`);
+
+--
+-- Indizes für die Tabelle `nachhilfesuche`
+--
+ALTER TABLE `nachhilfesuche`
+  ADD PRIMARY KEY (`sucheID`);
+
+--
+-- Indizes für die Tabelle `todoliste`
+--
+ALTER TABLE `todoliste`
+  ADD PRIMARY KEY (`todoID`);
+
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indizes für die Tabelle `todolist`
+-- AUTO_INCREMENT für exportierte Tabellen
 --
-ALTER TABLE `todolist`
-  ADD UNIQUE KEY `aufgabe` (`aufgabe`);
+
+--
+-- AUTO_INCREMENT für Tabelle `aufgabe`
+--
+ALTER TABLE `aufgabe`
+  MODIFY `aufgabenID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `kurs`
+--
+ALTER TABLE `kurs`
+  MODIFY `kursID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `nachhilfeangebot`
+--
+ALTER TABLE `nachhilfeangebot`
+  MODIFY `angebotID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `nachhilfesuche`
+--
+ALTER TABLE `nachhilfesuche`
+  MODIFY `sucheID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `todoliste`
+--
+ALTER TABLE `todoliste`
+  MODIFY `todoID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
